c8bff47fce5392ce8d6b2d662a5e086f3ad59aa5
Elithon Silva <elithonsilva@gmail.com>
2022-04-27 18:42:54 -0300

    cria associacoes com os models Sale e Product

--- back-end/src/database/models/SaleProduct.js
+++ back-end/src/database/models/SaleProduct.js
@@ -26,5 +26,20 @@ module.exports = (sequelize,
 
     });
 
+  SaleProduct.associate = (models) => {
+    models.Sale.belongsToMany(models.Product, {
+      as: 'sales',
+      through: SaleProduct,
+      foreignKey: 'sale_id',
+      otherKey: 'product_id',
+    });
+    models.Product.belongsToMany(models.Sale, {
+      as: 'products',
+      through: SaleProduct,
+      foreignKey: 'product_id',
+      otherKey: 'sale_id',
+    });
+  };
+
   return SaleProduct;
 };
