42077163d066afaf0a7a05161251a9474e8e6ed5
Elithon Silva <elithonsilva@gmail.com>
2022-05-02 20:07:31 -0300

    ajuste de lint no require do jwt

--- back-end/src/controllers/loginController.js
+++ back-end/src/controllers/loginController.js
@@ -1,6 +1,6 @@
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET;
 const jwtConfig = {
   expiresIn: '1d',
