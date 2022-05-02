76a8eb8127f9a05fc3c0faf88b929c90fc35d439
Paulo França <paulovinicius.franca@gmail.com>
2022-05-02 14:10:03 -0300

    Cria o component Content.jsx

---
+++ front-end/components/Content.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+
+function Content() {
+  return (
+    <Switch>
+      <Route exact path='/' component={ Login } />
+      <Route exact path='/subscribe' component={ Register } />
+    </Switch>
+  )
+}
+
+export default Content;
