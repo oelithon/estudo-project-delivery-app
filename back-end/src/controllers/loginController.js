e380d9043935370939768c69d5a7e19bbadacbe4
Elithon Silva <elithonsilva@gmail.com>
2022-05-02 18:29:01 -0300

    implementa loginController

---
+++ back-end/src/controllers/loginController.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
+const jwtConfig = {
+  expiresIn: '1d',
+  algorithm: 'HS256',
+};
+
+const { User } = require('../services/loginService');
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const userData = await User(email, password);
+
+  const token = jwt.sign({ userData }, secret, jwtConfig);
+
+  return res.status(200).json({ token });
+};
+
+module.exports = {
+  loginUser,
+};
