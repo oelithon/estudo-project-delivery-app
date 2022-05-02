15bff8008586625f3cc07687c8debd1ddce832ef
bel <mariaisabelalbuquerque@gmail.com>
2022-05-02 20:25:40 -0300

    fix: deleta arquivos

--- back-end/src/index.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-import app from './app';
-import connectToDatabase from './connection';
-
-const PORT = process.env.API_PORT || 3001;
-connectToDatabase()
-app.listen(PORT, () => console.log('ouvindo porta 3001!'));
