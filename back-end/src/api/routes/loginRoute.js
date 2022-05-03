4e172deab4ba99903ada8899fcd130b9ce30246f
Elithon Silva <elithonsilva@gmail.com>
2022-05-03 16:52:42 -0300

    configura rota /login com o controller.loginUser

--- back-end/src/api/routes/loginRoute.js
+++ back-end/src/api/routes/loginRoute.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const loginController = require('../controllers/loginController');
+
 const router = express.Router();
 
 router
-  .route('/login');
+  .route('/login')
+  .get((_req, res) => res.status(200).json({ message: 'tudo ok!' }))
+  .post(loginController.loginUser);
 
 module.exports = router;
