e8667281be664c367a1ce4bb7c39f59cfdc52576
perchespierri <perchespierri@gmail.com>
2022-05-03 10:23:41 -0300

    feat: Dropdown Menu Input

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Content from './components/Content';
 import './App.css';
+import './styles/global.css';
 
 function App() {
   return (
--- front-end/src/components/Content.jsx
+++ front-end/src/components/Content.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import DropdownMenu from './DropdownMenu';
 
 function Content() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/componentsview" component={ DropdownMenu } />
       <Route exact path="/subscribe" component={ Register } />
     </Switch>
   );
