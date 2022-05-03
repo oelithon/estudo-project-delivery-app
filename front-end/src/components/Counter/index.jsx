9c4507d99663d74ebab5937860fa07ead6364833
perchespierri <perchespierri@gmail.com>
2022-05-03 12:48:13 -0300

    feat: Counter button

--- front-end/src/components/Counter/index.jsx
+++ front-end/src/components/Counter/index.jsx
@@ -3,17 +3,21 @@ import './style.css';
 
 const Counter = () => {
   const [quantity, setQuantity] = useState(0);
+
   const handleClick = (value) => {
     if (value === '+') setQuantity((prevQuantity) => prevQuantity + 1);
     else if (value === '-' && quantity === 0) console.log('0 é o mínimo');
     else setQuantity((prevQuantity) => prevQuantity - 1);
   };
+
   const handleChange = (value) => {
     setQuantity(parseInt(value, 10));
   };
+
   useEffect(() => {
     if (Number.isNaN(quantity)) setQuantity(0);
   }, [quantity]);
+
   return (
     <div className="Counter__div">
       <button
