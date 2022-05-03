a04fa2286040fa8caec318a77ca704c720847729
perchespierri <perchespierri@gmail.com>
2022-05-03 16:02:58 -0300

    feat Card de produto

--- front-end/src/components/ProductCard/index.jsx
+++ front-end/src/components/ProductCard/index.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
-import DropdownMenu from '../DropdownMenu';
+import Counter from '../Counter';
 import './style.css';
+import imgURL from './skol_lata_350ml.jpg';
 
 const ProductCard = () => {
-  const name = 'SKOL PILSEN';
+  const price = 'R$ 2,20';
+  const description = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.';
+
   return (
     <div className="ProductCard__div">
-      <div className="productName">
-        {name}
+      <div className="image-container">
+        <img
+          src={ imgURL }
+          alt="Product logo"
+          className="productImage"
+        />
       </div>
-      <div className="productNameAndImage">
-        <div className="productPrice">PREÇO</div>
-        IMAGEM DO PRODUTO
+      <div className="productPrice">
+        { price }
       </div>
-      <div className="productQuantityAndDescription">
-        <div className="productDescription">Descrição</div>
-        <DropdownMenu />
+      <div className="productDescriptionAndCounter">
+        { description }
+        <div className="productCounter">
+          <Counter />
+        </div>
       </div>
     </div>
   );
