c48ae4147101214b0232b15b160f3fc1d8d110e0
bel <mariaisabelalbuquerque@gmail.com>
2022-05-03 16:08:30 -0300

    fix: corrige rota POST /register

--- back-end/src/api/controllers/userController.js
+++ back-end/src/api/controllers/userController.js
@@ -2,7 +2,6 @@ const { createUser: create } = require('../services/userService');
 
 const createUser = async (req, res) => {
   const { status, json } = await create(req.body);
-  console.log('status=====>', status, 'json===>', json);
   return res.status(status).json(json);
 };
 
--- back-end/src/api/services/userService.js
+++ back-end/src/api/services/userService.js
@@ -1,8 +1,9 @@
 const { User } = require('../../database/models');
+const { emailAlreadyRegistered } = require('../helpers/errorMessages');
 const { generateToken } = require('../helpers/jwt');
 const { encryptPassword } = require('../helpers/md5');
 const { errorResponse, goodResponse } = require('../helpers/response');
-const { CREATED, INTERNAL_SERVER_ERROR } = require('../helpers/statusCode');
+const { CREATED, INTERNAL_SERVER_ERROR, BAD_REQUEST } = require('../helpers/statusCode');
 
 const loginUser = async (email, password) => {
   const userData = await User.findOne({ where: { email, password } });
@@ -10,13 +11,21 @@ const loginUser = async (email, password) => {
   return userData;
 };
 
+const checkEmail = async (email) => {
+  const user = await User.findOne({ where: { email } });
+  return !user === false;
+};
+
 const createUser = async (body) => {
   const user = body;
+  user.password = encryptPassword(body.password);
   try {
-    user.password = encryptPassword(body.password);
-    const created = await User.create(user);
-    const token = generateToken(created.id);
-    return goodResponse(CREATED, { ...created.dataValues, token });
+    if (!await checkEmail(user.email)) {
+      const created = await User.create(user);
+      const token = generateToken(created.id);
+      return goodResponse(CREATED, { ...created.dataValues, token });
+    }
+    return errorResponse(BAD_REQUEST, emailAlreadyRegistered);
   } catch (err) {
     return errorResponse(INTERNAL_SERVER_ERROR, err);
   }
