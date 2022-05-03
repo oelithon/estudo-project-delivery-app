b6b4dbdc6b826ddfced004e2cf49a5d2c778e535
perchespierri <perchespierri@gmail.com>
2022-05-03 12:06:51 -0300

    Teste commit

--- front-end/src/components/Content.jsx
+++ front-end/src/components/Content.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
-import DropdownMenu from './DropdownMenu';
+import Counter from './Counter';
 
 function Content() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route exact path="/componentsview" component={ DropdownMenu } />
+      <Route exact path="/componentsview" component={ Counter } />
       <Route exact path="/subscribe" component={ Register } />
     </Switch>
   );
--- front-end/src/components/index.js
+++ front-end/src/components/index.js
@@ -1,3 +1,4 @@
 import DropdownMenu from './DropdownMenu';
+import Counter from './Counter';
 
-export { DropdownMenu };
+export { DropdownMenu, Counter };
