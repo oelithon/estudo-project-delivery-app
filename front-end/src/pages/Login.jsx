b0041d78e64df660138cb8695a2964afa7a55870
Paulo França <paulovinicius.franca@gmail.com>
2022-05-03 16:19:22 -0300

    Cria o LoginContext.js

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 import { Button, Input } from '../components';
 import logo from '../images/Zeca.png';
 import '../styles/Login.css';
+import '../styles/global.css';
 
 function Login() {
+  // const [email, setEmail] = useState({
+  //   user: { email: '' },
+  // });
+
+  // const [password, setPassword] = useState({
+  //   password: '',
+  // });
+
+  // const settingEmail = ({ target }) => {
+  //   setEmail({
+  //     user: { email: target.value },
+  //   });
+  // };
+  // const settingPassword = ({ target }) => {
+  //   setPassword({ password: target.value });
+  // };
+  // const passwordMinLength = 6;
+  // const size
+  // const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  // const isValid = regex.test()
+  // const enabled = isValid && size > passwordMinLength;
+
   return (
     <div>
       <main className="Login-main">
@@ -17,6 +41,7 @@ function Login() {
             className="default"
             dataTestId="common_login__input-email"
             type="text"
+            onChange={ settingEmail }
             placeholder="email@trybeer.com.br"
           />
           <Input
@@ -24,6 +49,7 @@ function Login() {
             className="default"
             dataTestId="common_login__input-password"
             type="password"
+            onChange={ settingPassword }
             placeholder="**********"
           />
           <Button
