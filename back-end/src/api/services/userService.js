d87d861ae8b13c3ea8ab60518c752c5622a57bd6
bel <mariaisabelalbuquerque@gmail.com>
2022-05-03 13:39:33 -0300

    feat: implementa a rota POST /register

--- back-end/src/api/services/userService.js
+++ back-end/src/api/services/userService.js
@@ -1,4 +1,8 @@
 const { User } = require('../../database/models');
+const { generateToken } = require('../helpers/jwt');
+const { encryptPassword } = require('../helpers/md5');
+const { errorResponse, goodResponse } = require('../helpers/response');
+const { CREATED, INTERNAL_SERVER_ERROR } = require('../helpers/statusCode');
 
 const loginUser = async (email, password) => {
   const userData = await User.findOne({ where: { email, password } });
@@ -6,6 +10,19 @@ const loginUser = async (email, password) => {
   return userData;
 };
 
+const createUser = async (body) => {
+  const user = body;
+  try {
+    user.password = encryptPassword(body.password);
+    const createdUser = await User.create(user);
+    const token = generateToken(createdUser);
+    return goodResponse(CREATED, token);
+  } catch (err) {
+    return errorResponse(INTERNAL_SERVER_ERROR, err);
+  }
+};
+
 module.exports = {
   loginUser,
+  createUser,
 };
