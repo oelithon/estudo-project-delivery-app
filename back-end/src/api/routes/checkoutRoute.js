881b03abd0ac8c2c7af9684466d7a376ff4290dc
Elithon Silva <elithonsilva@gmail.com>
2022-05-05 17:55:55 -0300

    configura .post(checkoutController.create) na rota /checkout

--- back-end/src/api/routes/checkoutRoute.js
+++ back-end/src/api/routes/checkoutRoute.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router
   .route('/checkout')
-  .get(checkoutController.seller);
+  .get(checkoutController.seller)
+  .post(checkoutController.create);
 
 module.exports = router;
