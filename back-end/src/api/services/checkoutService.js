34c8ff4e8218f31cf047ced17209d5ac72dbc3d3
Elithon Silva <elithonsilva@gmail.com>
2022-05-05 15:39:32 -0300

    implementa a camada checkoutService com função sellerList que retorna lista de vendedores

---
+++ back-end/src/api/services/checkoutService.js
@@ -0,0 +1,13 @@
+const { User } = require('../../database/models');
+const { goodResponse } = require('../helpers/response');
+const statusCode = require('../helpers/statusCode');
+
+const sellerList = async () => {
+  const list = await User.findAll({ where: { role: 'customer' } });
+
+  return goodResponse(statusCode.OK, list);
+}
+
+module.exports = {
+  sellerList,
+}
