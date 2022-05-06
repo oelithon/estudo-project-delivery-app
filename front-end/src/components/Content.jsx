db3fc79e993ffcff060b686b1f634421c901876f
perchespierri <perchespierri@gmail.com>
2022-05-06 14:15:08 -0300

    Apaga linhas necessárias

--- front-end/src/components/Content.jsx
+++ front-end/src/components/Content.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
-import DeliveryCard from './DeliveryCard';
 
 function Content() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route exact path="/componentsview" component={ DeliveryCard } />
+      {/* <Route exact path="/componentsview" component={ DeliveryCard } /> */}
       <Route exact path="/subscribe" component={ Register } />
     </Switch>
   );
