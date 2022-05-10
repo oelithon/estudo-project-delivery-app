12b86696fe1b0adb09a5a296701d5907e8eeb9d1
bel <mariaisabelalbuquerque@gmail.com>
2022-05-10 17:02:19 -0300

    feat: implementa rota GET /users

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -2,10 +2,12 @@ const registerRouter = require('./registerRoute');
 const costumerRoute = require('./costumerRoute');
 const loginRoute = require('./loginRoute');
 const checkoutRoute = require('./checkoutRoute');
+const usersRoute = require('./usersRoute');
 
 module.exports = [
   registerRouter,
   loginRoute,
   costumerRoute,
   checkoutRoute,
+  usersRoute,
 ];
