cce03607fb3df1af65f268a859b884e1d271c9c0
perchespierri <perchespierri@gmail.com>
2022-05-12 18:52:38 -0300

    Importa Link em Products

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 import { Navbar, ProductCard } from '../components';
 import '../styles/Products.css';
 import formatNumbertoBRL from '../helpers/formatNumberToBRL';
