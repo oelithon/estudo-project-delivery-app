07989e30c65d0cf3e7838336783de2f507929d31
Paulo França <paulovinicius.franca@gmail.com>
2022-05-12 13:18:02 -0300

    Devolve a rota com Navigate

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import LoginProvider from './context/LoginProvider';
 import { Login, Register, Checkout, Products, SalesTest } from './pages';
 import './styles/global.css';
@@ -9,13 +9,13 @@ function App() {
     <BrowserRouter>
       <LoginProvider>
         <Routes>
-          <Route path="/" element={ <Login /> } />
           <Route path="/login" element={ <Login /> } />
           <Route path="/register" element={ <Register /> } />
           <Route path="/subscribe" element={ <Register /> } />
           <Route path="/customer/checkout" element={ <Checkout /> } />
           <Route path="/customer/orders/:id" element={ <SalesTest /> } />
           <Route path="/customer/products" element={ <Products /> } />
+          <Route exact path="/" element={ <Navigate to="/login" /> } />
         </Routes>
       </LoginProvider>
     </BrowserRouter>
