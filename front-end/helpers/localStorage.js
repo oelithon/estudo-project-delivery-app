56385d56e10d13bf6c11b305dee826da76d90d5e
bel <mariaisabelalbuquerque@gmail.com>
2022-05-12 13:34:32 -0300

    fix: corrige lint

--- front-end/helpers/localStorage.js
+++ front-end/helpers/localStorage.js
@@ -1,5 +1,5 @@
 const USER_KEY = 'user';
-const PRODUCTS_KEY = 'products'
+const PRODUCTS_KEY = 'products';
 
 const emptyUser = {
   name: '',
@@ -8,39 +8,35 @@ const emptyUser = {
   token: '',
 };
 
-// função para remover qualquer coisa do localStorage, 
+// função para remover qualquer coisa do localStorage,
 // só passar o nome da chave que quer deletar
-const removeLocalStorage = (key) => localStorage.removeItem(key)
-
+export const removeLocalStorage = (key) => localStorage.removeItem(key);
 
 // funçõe para ler info de user do localStorage
-const readUser = () => {
+export const readUser = () => {
   if (!JSON.parse(localStorage.getItem(USER_KEY))) {
     localStorage.setItem(USER_KEY, JSON.stringify(emptyUser));
   }
-  return JSON.parse(localStorage.getItem(USER_KEY))
+  return JSON.parse(localStorage.getItem(USER_KEY));
 };
 
-
 // funçõe para salvar info de user do localStorage
-const saveUser = (user) => localStorage.setItem(USER_KEY, JSON.stringify(user));
-
+export const saveUser = (user) => localStorage.setItem(USER_KEY, JSON.stringify(user));
 
 // função para ler produtos no localStorage
 const readProducts = () => {
   if (!JSON.parse(localStorage.getItem(PRODUCTS_KEY))) {
     localStorage.setItem(PRODUCTS_KEY, JSON.stringify([]));
   }
-  return JSON.parse(localStorage.getItem(PRODUCTS_KEY))
+  return JSON.parse(localStorage.getItem(PRODUCTS_KEY));
 };
 
 const saveProducts = (products) => {
   localStorage.setItem(PRODUCTS_KEY, JSON.stringify(products));
-  return readProducts()
-}
+  return readProducts();
+};
 
-
-// função para adicionar um produto no localStorage, 
+// função para adicionar um produto no localStorage,
 // ela tb retorna os produtos do localStorage
 export const addProductInLocalStorage = (product) => {
   if (product) {
