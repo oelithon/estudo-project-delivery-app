b1a6777f373314431601a1a66660d21f950f0d00
Elithon Silva <elithonsilva@gmail.com>
2022-05-13 15:29:36 -0300

    mearge com a branch main-group-1-products-sum-card

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, ItemBox, QuantityBox,
   PriceBox, SubTotalBox, DescriptionBox, TotalBox, Input, Navbar } from '../components';
